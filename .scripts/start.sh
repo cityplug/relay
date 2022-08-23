bb96b22fba009df66bfa3591834d2265aafda89b
cityplug <seni@cityplug.co.uk>
2022-08-23 11:55:14 +0100

    .

--- .scripts/start.sh
+++ .scripts/start.sh
@@ -58,7 +58,6 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-echo
 
 apt update && apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
