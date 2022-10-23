6e71a41179ab2661528949c0ea2120b5502dc474
cityplug <seni@cityplug.co.uk>
2022-10-23 14:25:07 +0100

    .

--- .scripts/start.sh
+++ .scripts/start.sh
@@ -35,7 +35,7 @@ passwd root
 echo "#  ---  Root password changed  ---  #"
 
 # --- Initialzing relay
-hostnamectl set-hostname relay.home.loc
+hostnamectl set-hostname relay.home.lan
 hostnamectl set-hostname "Relay Host Machine" --pretty
 rm -rf /etc/hosts
 mv /opt/relay/.scripts/hosts /etc/hosts
